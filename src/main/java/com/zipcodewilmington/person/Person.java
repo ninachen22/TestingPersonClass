c858079ab031cf3e5712660444d08a3d5cca7568
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-11-01 19:09:38 -0400

    made changes

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,10 +4,11 @@ package com.zipcodewilmington.person;
  * Created by leon on 2/12/18.
  */
 public class Person {
-    private String name;
-    private int age;
+    private String name = "Leon";
+    private int age = 5;
 
     public Person() {
+
     }
 
     public Person(int age) {
@@ -20,16 +21,18 @@ public class Person {
     }
 
     public void setName(String name) {
+        System.out.println(name);
     }
 
     public void setAge(int age) {
+        System.out.println(age);
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
